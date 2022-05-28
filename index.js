5d479158ab56ec6e0ff785b04bb97121fea66524
Jose Soto <jsoto7087@gmail.com>
2022-05-28 18:00:29 -0400

    commit tree 4546c37f2adc2bd86fdb55fec1659fecfb6f4014

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+let currentUser = "Grace Hopper"
+// let currentUser = `Welcome to Flatbook, `;
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}` + "!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}` + "!";
+
+
+
+
+
+
+
+
+
+
+
+
+// const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+// const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
